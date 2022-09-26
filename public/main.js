6852fc0e786929629bec8b1f526082d9f186bea7
kaveeshadinamidu <kaveeshadinamidu1999@gmail.com>
2022-09-26 14:17:55 +0530

    View size changed

--- public/main.js
+++ public/main.js
@@ -6,8 +6,8 @@ const isDev = require("electron-is-dev");
 function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1920,
+    height: 1080,
     webPreferences: {
       nodeIntegration: true,
     },
